77d8c090ce52d0b40f9f418bf5b2dcebc46bd93c
awilsonbahun <111086903+awilsonbahun@users.noreply.github.com>
2022-11-10 19:23:10 -0500

    Create RecursionTester.java

---
+++ RecursionTester.java
@@ -0,0 +1,17 @@
+public class RecursionTester {
+    public static void main(String[] args) {
+        Recursion1 r = new Recursion1();
+        r.displayString("ggc", 2);
+        r.getMultiChar('a', 1);
+        r.factorial(2);
+        r.getPowerOfTen(2);
+        r.getPowerOfTen(2);
+        r.numberOfGloves(5);
+        r.getNumberOfZerosRecursion(3);
+        r.getNumberOfZerosLoop(4);
+
+        System.out.println(r);
+
+
+    }
+}
